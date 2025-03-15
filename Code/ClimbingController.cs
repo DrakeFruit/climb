ad04bfd9cc2c6c148fc9177f9e7132c0ca8554d2
DrakeFruit <94070205+DrakeFruit@users.noreply.github.com>
2025-03-14 20:07:00 -0700

    multiplayer

--- Code/ClimbingController.cs
+++ Code/ClimbingController.cs
@@ -1,3 +1,4 @@
+using System;
 using Sandbox;
 using Sandbox.Physics;
 
@@ -10,12 +11,30 @@ public sealed class ClimbingController : Component
 	PhysicsBody CursorBody { get; set; }
 	Rigidbody HammerBody { get; set; }
 	Rigidbody PlayerBody { get; set; }
+	PhysicsSpring TempSpring { get; set; }
 	protected override void OnStart()
 	{
-		Mouse.Visible = true;
+		if ( IsProxy )
+		{
+			foreach ( var i in Components.GetAll<Rigidbody>() )
+			{
+				Log.Info( i  );
+				i.Enabled = false;
+			}
+			foreach ( var i in Components.GetAll<Collider>() )
+			{
+				Log.Info( i  );
+				i.Enabled = false;
+			}
+
+			return;
+		}
+		
 		HammerBody = Hammer.GetComponent<Rigidbody>();
 		CursorBody = new PhysicsBody( Scene.PhysicsWorld ) { BodyType = PhysicsBodyType.Keyframed };
 		PlayerBody = Components.Get<Rigidbody>();
+		
+		Mouse.Visible = true;
 
 		PhysicsPoint point1 = new( CursorBody );
 		PhysicsPoint point2 = new( HammerBody.PhysicsBody, Head.LocalPosition );
@@ -23,28 +42,29 @@ public sealed class ClimbingController : Component
 		GrabJoint.Point1 = point1;
 		GrabJoint.Point2 = point2;
 		
-		var maxForce = 50 * HammerBody.Mass * Scene.PhysicsWorld.Gravity.Length;
+		var maxForce = 100 * HammerBody.Mass * Scene.PhysicsWorld.Gravity.Length;
 		GrabJoint.SpringLinear = new PhysicsSpring( 15, 2, maxForce );
 		GrabJoint.SpringAngular = new PhysicsSpring( 15, 2, 0 );
+		TempSpring = GrabJoint.SpringLinear;
 
-		HammerBody.GameObject.Flags = HammerBody.GameObject.Flags.WithFlag( GameObjectFlags.Absolute, true );
+		Hammer.Flags = Hammer.Flags.WithFlag( GameObjectFlags.Absolute, true );
 	}
 	
 	protected override void OnFixedUpdate()
 	{
-		var tr = Scene.Trace.Ray( Scene.Camera.ScreenPixelToRay( Mouse.Position ), 1000 ).Run();
-		
-		//Movement
-		var newPos = tr.EndPosition.WithY( 0 );
-		CursorBody.Position = newPos;
+		if ( IsProxy ) return;
 
-		var dir = Vector3.Direction( HammerBody.WorldPosition, tr.EndPosition.WithY( 0 ) ).Normal; 
-		var colTr = Scene.Trace.Ray( HammerBody.WorldPosition, HammerBody.WorldPosition + dir * 32 )
-            .IgnoreGameObjectHierarchy( GameObject )
-            .Run();
-		if ( colTr.Hit )
+		Scene.Camera.WorldPosition = WorldPosition.WithY( -512 ) + Vector3.Zero.WithZ( 64 );
+		var mouseTr = Scene.Trace.Ray( Scene.Camera.ScreenPixelToRay( Mouse.Position ), 1000 ).Run();
+		CursorBody.Position = mouseTr.EndPosition.WithY( 0 );
+		HammerBody.ApplyForce( PlayerBody.Velocity );
+
+		if ( HammerBody.Touching.Any() )
 		{
-			PlayerBody.Velocity += -colTr.Direction.Normal.WithY( 0 ) * PlayerBody.Mass * Time.Delta / 4;
+			var dir = Head.WorldPosition - CursorBody.Position;
+			var force = dir.Normal * MathF.Pow( dir.Length, 2 );
+			force = force.ClampLength( 0, 125 );
+			PlayerBody.ApplyForceAt( Head.WorldPosition, force * PlayerBody.Mass * 25 );
 		}
 	}
 }
--- Code/CustomSlider.cs
+++ Code/CustomSlider.cs
@@ -3,13 +3,29 @@ using Sandbox.Physics;
 
 public sealed class CustomSlider : Component
 {
-	[Property] int Length;
-	[Property] Rigidbody Body;
+	[Property] Rigidbody Body { get; set; }
+	[Property] int Length { get; set; }
+	[Property] Vector3 Offset { get; set; }
+	public Rigidbody SelfBody { get; set; }
 	protected override void OnStart()
 	{
-		Rigidbody rb = Components.Get<Rigidbody>();
-		PhysicsJoint joint = PhysicsJoint.CreateLength( new PhysicsPoint( rb.PhysicsBody ), new PhysicsPoint( Body.PhysicsBody ), Length );
-		joint.Point1 = new PhysicsPoint( rb.PhysicsBody );
+		if ( IsProxy ) return;
+		
+		SelfBody = Components.Get<Rigidbody>();
+		var phys1 = new PhysicsPoint(SelfBody.PhysicsBody, SelfBody.WorldPosition + Offset);
+		var phys2 = new PhysicsPoint(Body.PhysicsBody);
+		PhysicsJoint joint = PhysicsJoint.CreateLength( phys1, phys2, Length );
+		joint.Point1 = new PhysicsPoint( SelfBody.PhysicsBody, Offset );
 		joint.Point2 = new PhysicsPoint( Body.PhysicsBody );
+	}
+	protected override void OnFixedUpdate()
+	{
+		if ( IsProxy ) return;
+		
+		var dir = Body.WorldPosition - WorldPosition;
+		if ( dir.Length > Length )
+		{
+			
+		}
 	}
 }
